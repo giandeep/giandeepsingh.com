9217de5a7afc639019087d715ad4a5860f44d61d
giandeep <rock.gold0076@gmail.com>
2022-07-02 15:20:26 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openLinkdin() {
+    window.open("https://www.linkedin.com/in/giandeep-singh");
+}
+
+function openBehance() {
+    window.open("https://www.behance.net/giandeep-singh");
+}
+
+function openResume() {
+    window.open("GiandeepSinghResumeJuly2022.pdf");
+}
